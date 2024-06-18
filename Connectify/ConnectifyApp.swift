f5a3bae2269dc2285636d3913f58ab89ff7662b7
Tristan Chay <tristanchay123@gmail.com>
2024-06-18 11:27:16 +0800

    Initial Commit

---
+++ Connectify/ConnectifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConnectifyApp.swift
+//  Connectify
+//
+//  Created by Tristan Chay on 18/6/24.
+//
+
+import SwiftUI
+
+@main
+struct ConnectifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Connectify/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Connectify
+//
+//  Created by Tristan Chay on 18/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
